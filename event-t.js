e39a67833f83b68fbd0bda33c28391704f77f3de
theNkennaAmadi <108593101+theNkennaAmadi@users.noreply.github.com>
2023-05-12 12:53:29 +0100

    Update event-t.js

--- event-t.js
+++ event-t.js
@@ -6,3 +6,13 @@ document.querySelector(".back-btn").addEventListener("mouseenter", () => {
 document.querySelector(".back-btn").addEventListener("mouseleave", () => {
   arrow.classList.remove("move");
 });
+
+const eventDates = [...document.querySelectorAll(".event-sub-block")];
+
+eventDates.map((date) => {
+  if (
+    date.querySelector("[event-start]").textContent === date.querySelector("[event-end]").textContent
+  ) {
+    date.querySelector("[event-end]").remove();
+  }
+});
